e1bdec3dd6048bec2939f634273f6ea426ae1b52
noboKumar <nobokumar227@gmail.com>
2025-03-01 17:01:27 +0600

    add main board plus minus

--- scripts/script.js
+++ scripts/script.js
@@ -17,10 +17,46 @@ function currentDate() {
   const month = date.getMonth();
   const year = date.getFullYear();
 
-  const monthName = ["jan", "feb", "mar", "apr", "may", "jun", "july", "aug", "sep", "oct", "nov", "dec"];
+  const monthName = [
+    "jan",
+    "feb",
+    "mar",
+    "apr",
+    "may",
+    "jun",
+    "july",
+    "aug",
+    "sep",
+    "oct",
+    "nov",
+    "dec",
+  ];
   const weekName = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 
-  document.getElementById("week").innerText = `${weekName[week]}`
-  document.getElementById("date-month-year").innerText = `${monthName[month]} ${day} ${year}`;
+  document.getElementById("week").innerText = `${weekName[week]}`;
+  document.getElementById(
+    "date-month-year"
+  ).innerText = `${monthName[month]} ${day} ${year}`;
 }
 currentDate();
+
+// complete button
+function completeButton() {
+  const completeBtn = document.getElementsByClassName("complete-btn");
+  let taskAssign = Number(document.getElementById("task-assigned").innerText) - 1;
+  let totalTask = Number(document.getElementById("total-task").innerText) + 1;
+
+  for (const button of completeBtn) {
+    button.addEventListener("click", function () {
+      alert("Board Updated Successfully")
+      document.getElementById("task-assigned").innerText = `0${taskAssign--}`;
+      document.getElementById("total-task").innerText = `${totalTask++}`;
+      button.disabled = true;
+
+      if (taskAssign === -1) {
+        alert("congrats!!! You Have Completed all the current Task");
+      }
+    });
+  }
+}
+completeButton();
