aa699fe49c0466dfd90a7899d5b25771bb4b6661
noboKumar <nobokumar227@gmail.com>
2025-03-01 13:21:37 +0600

    add blog js button and linked

---
+++ scripts/blog.js
@@ -0,0 +1,4 @@
+// back to desk button
+document.getElementById("back-to-desk-btn").addEventListener("click", function () {
+    window.location.href = "./index.html";
+});
---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+// dicover new button
+document.getElementById("discover-btn").addEventListener("click", function () {
+  window.location.href = "./blog.html";
+});
